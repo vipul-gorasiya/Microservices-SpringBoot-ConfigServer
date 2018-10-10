ef66a739614402a649902355ea01ba0fe7b2bade
vipul-gorasiya <39234040+vipul-gorasiya@users.noreply.github.com>
2018-10-10 09:58:34 +0530

    added refresh scope to enable config refresh

--- Spring-Ms-Config-Client/src/main/java/com/vipul/cloud/SpringMsConfigClientApplication.java
+++ Spring-Ms-Config-Client/src/main/java/com/vipul/cloud/SpringMsConfigClientApplication.java
@@ -2,8 +2,11 @@ package com.vipul.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+// To enable refresh of config properties being accessed from config server
+@RefreshScope
 public class SpringMsConfigClientApplication {
 
 	public static void main(String[] args) {
